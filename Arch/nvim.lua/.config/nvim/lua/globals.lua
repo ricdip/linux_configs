50ea69f0e7d52234384be5c161bb96db3fa9589e
ricdip <riccardodpr@gmail.com>
2022-11-29 23:30:48 +0100

    Arch

--- Arch/nvim.lua/.config/nvim/lua/globals.lua
+++ Arch/nvim.lua/.config/nvim/lua/globals.lua
@@ -7,13 +7,13 @@ vim.g.mapleader = " "
 -- Treesitter parsers
 helper.coding.treesitter.parsers = {
     "python", "c", "cpp", "go", "java", "lua", "bash", "javascript",
-    "typescript", "json", "html", "css", "markdown", "dockerfile", "regex"
+    "typescript", "json", "html", "css", "markdown", "dockerfile", "regex", "rust"
 }
 
--- LSP language servers: Python, C/C++, Go, Java, Lua, Bash, JavaScript/TypeScript, JSON, HTML, CSS, Markdown, Dockerfile
+-- LSP language servers: Python, C/C++, Go, Java, Lua, Bash, JavaScript/TypeScript, JSON, HTML, CSS, Markdown, Dockerfile, Rust
 helper.coding.lsp.language_servers = {
     "pyright", "clangd", "gopls", "jdtls", "sumneko_lua", "bashls", "tsserver",
-    "jsonls", "html", "cssls", "marksman", "dockerls"
+    "jsonls", "html", "cssls", "marksman", "dockerls", "rust_analyzer"
 }
 
 -- LSP diagnostics colors
