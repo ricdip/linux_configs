695573a7b3974a3c4d5c2867616b22348bf910fc
ricdip <riccardodpr@gmail.com>
2024-02-04 21:56:56 +0100

    nixos: config

--- NixOS/home/programs/i3.nix
+++ NixOS/home/programs/i3.nix
@@ -2,12 +2,22 @@
 
 {
   # i3 config
-  xsession.windowManager.i3.enable = true;
-  xsession.windowManager.i3.config = {
-    # Alt key
-    modifier = "Mod1";
-    terminal = "alacritty";
-    # i3status bar
-    bars = [{ position = "top"; }];
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = {
+      # Alt key
+      modifier = "Mod1";
+      terminal = "alacritty";
+      # i3status bar
+      bars = [{ position = "top"; }];
+      # default workspace
+      defaultWorkspace = "workspace number 1";
+    };
+  };
+
+  # i3status config
+  programs.i3status = {
+    enable = true;
+    enableDefault = true;
   };
 }
