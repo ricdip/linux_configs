6723376618336b04ed9836d54dc06e5b793405d2
ricdip <riccardodpr@gmail.com>
2024-02-04 23:05:21 +0100

    nixos: config

--- NixOS/home/programs/common.nix
+++ NixOS/home/programs/common.nix
@@ -2,5 +2,5 @@
 
 {
   # packages that should be installed to the user profile
-  home.packages = with pkgs; [ neofetch zip unzip btop nnn ];
+  home.packages = with pkgs; [ neofetch zip unzip btop nnn just ];
 }
