a60628966690306b0ca6994f9ebb4c057c19e864
ricdip <riccardodpr@gmail.com>
2024-02-04 23:34:09 +0100

    nixos: config

--- NixOS/home/programs/i3.nix
+++ NixOS/home/programs/i3.nix
@@ -12,7 +12,7 @@
       bars = [{
         position = "top";
         statusCommand =
-          "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
+          "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
       }];
       # default workspace
       defaultWorkspace = "workspace number 1";
@@ -20,5 +20,6 @@
   };
 
   # i3status-rust config
+  # TODO: custom config
   programs.i3status-rust = { enable = true; };
 }
