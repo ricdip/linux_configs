86360c5614577cf1457a2b052f5a67d017a48305
ricdip <riccardodpr@gmail.com>
2024-02-20 19:58:58 +0100

    nixos: config

--- NixOS/home/programs/programming.nix
+++ NixOS/home/programs/programming.nix
@@ -1,10 +1,15 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ jdk17 ];
+  home.packages = with pkgs; [ jdk17 go ];
 
   programs.java = {
     enable = true;
     package = pkgs.jdk17;
+  };
+
+  programs.go = {
+    enable = true;
+    package = pkgs.go;
   };
 }
