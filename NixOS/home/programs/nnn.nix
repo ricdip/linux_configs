c186a61fd4208ec3511941153a4d7f18b0b4a2f8
ricdip <riccardodpr@gmail.com>
2024-05-10 00:23:44 +0200

    nixos: config

--- NixOS/home/programs/nnn.nix
+++ NixOS/home/programs/nnn.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  # environment variables for nnn
+  home.sessionVariables = { NNN_TMPFILE = "/tmp/.lastd"; };
+
   programs.nnn = {
     enable = true;
     package = pkgs.nnn.override ({ withNerdIcons = true; });
