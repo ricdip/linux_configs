250b50af7ce9adc8180e5d3823b3c63a59415f98
ricdip <riccardodpr@gmail.com>
2024-09-07 23:40:01 +0200

    nixos: config

--- NixOS/system/modules/virt.nix
+++ NixOS/system/modules/virt.nix
@@ -1,4 +1,11 @@
 {
+  # VMs
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+
+  # containers
+  # virtualisation.docker = {
+  #   enable = true;
+  #   package = pkgs.docker;
+  # };
 }
