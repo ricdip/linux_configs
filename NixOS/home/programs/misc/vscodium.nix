d2950fcce0eb0ff5664cd660cdb8696f3709d9a0
ricdip <riccardodpr@gmail.com>
2024-09-08 22:33:35 +0200

    nixos: config

--- NixOS/home/programs/misc/vscodium.nix
+++ NixOS/home/programs/misc/vscodium.nix
@@ -3,6 +3,13 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [ jnoortheen.nix-ide ];
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      astro-build.astro-vscode
+      eamodio.gitlens
+      oderwat.indent-rainbow
+      ecmel.vscode-html-css
+      bradlc.vscode-tailwindcss
+    ];
   };
 }
