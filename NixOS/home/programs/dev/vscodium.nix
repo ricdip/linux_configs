06dc418f9330fab3b6deda662a24d3719aab0e6d
ricdip <riccardodpr@gmail.com>
2025-03-05 00:09:31 +0100

    nixos: config

--- NixOS/home/programs/dev/vscodium.nix
+++ NixOS/home/programs/dev/vscodium.nix
@@ -4,12 +4,23 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
+      # nix
       jnoortheen.nix-ide
-      astro-build.astro-vscode
+      # utility
       eamodio.gitlens
       oderwat.indent-rainbow
+      grapecity.gc-excelviewer
+      # docs
+      bierner.markdown-mermaid
+      # frontend
       ecmel.vscode-html-css
       bradlc.vscode-tailwindcss
+      astro-build.astro-vscode
+      angular.ng-template
+      # programming languages
+      golang.go
+      ms-python.python
+      rust-lang.rust-analyzer
     ];
   };
 }
