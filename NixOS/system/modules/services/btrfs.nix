8fc04ffca25d0326f1d84e63f908f493a7e3af1d
ricdip <riccardodpr@gmail.com>
2025-03-11 22:31:10 +0100

    nixos: config

--- NixOS/system/modules/services/btrfs.nix
+++ NixOS/system/modules/services/btrfs.nix
@@ -12,15 +12,26 @@
     # cleanup interval: format https://www.freedesktop.org/software/systemd/man/latest/systemd.time.html
     cleanupInterval = "1d";
     configs = {
+      # remember to create subvolume /.snapshots (`sudo btrfs subvolume create /.snapshots`)
+      # root = {
+      #   # path of the subvolume or mount point
+      #   SUBVOLUME = "/";
+      #   # defines whether hourly snapshots should be created
+      #   TIMELINE_CREATE = true;
+      #   # defines whether the timeline cleanup algorithm should be run for the config
+      #   TIMELINE_CLEANUP = true;
+      #   # limits for timeline cleanup
+      #   TIMELINE_LIMIT_HOURLY = "2";
+      #   TIMELINE_LIMIT_DAILY = "1";
+      #   TIMELINE_LIMIT_WEEKLY = "0";
+      #   TIMELINE_LIMIT_MONTHLY = "0";
+      #   TIMELINE_LIMIT_YEARLY = "0";
+      # };
       # remember to create subvolume /home/.snapshots (`sudo btrfs subvolume create /home/.snapshots`)
       home = {
-        # path of the subvolume or mount point
         SUBVOLUME = "/home";
-        # defines whether hourly snapshots should be created
         TIMELINE_CREATE = true;
-        # defines whether the timeline cleanup algorithm should be run for the config
         TIMELINE_CLEANUP = true;
-        # limits for timeline cleanup
         TIMELINE_LIMIT_HOURLY = "5";
         TIMELINE_LIMIT_DAILY = "2";
         TIMELINE_LIMIT_WEEKLY = "1";
