bedc2b3fd3f4ef40466b69fd7c8de0cf9f4ee9b9
ricdip <riccardodpr@gmail.com>
2025-03-23 23:08:35 +0100

    nixos: config

--- NixOS/home/programs/misc/xdg.nix
+++ NixOS/home/programs/misc/xdg.nix
@@ -9,7 +9,28 @@
   xdg = {
     mimeApps = {
       defaultApplications = {
-        "text/*" = [
+        # application
+        "application/json" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "application/yaml" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "application/toml" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "application/xml" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "application/sql" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "application/x-tex" = [
           "Helix.desktop"
           "codium.desktop"
         ];
@@ -17,21 +38,96 @@
           "zathura.desktop"
           "firefox.desktop"
         ];
-        "image/*" = [
-          "qimgv.desktop"
-          "sxiv.desktop"
-        ];
-        "audio/*" = "mpv.desktop";
-        "video/*" = "mpv.desktop";
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-        "x-scheme-handler/chrome" = "firefox.desktop";
-        "text/html" = "firefox.desktop";
         "application/x-extension-htm" = "firefox.desktop";
         "application/x-extension-html" = "firefox.desktop";
         "application/x-extension-shtml" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
         "application/x-extension-xhtml" = "firefox.desktop";
         "application/x-extension-xht" = "firefox.desktop";
+        # audio
+        "audio/3gpp" = "mpv.desktop";
+        "audio/3gpp2" = "mpv.desktop";
+        "audio/aac" = "mpv.desktop";
+        "audio/ac3" = "mpv.desktop";
+        "audio/flac" = "mpv.desktop";
+        "audio/matroska" = "mpv.desktop";
+        "audio/mpa" = "mpv.desktop";
+        "audio/mp4" = "mpv.desktop";
+        "audio/mpeg" = "mpv.desktop";
+        "audio/ogg" = "mpv.desktop";
+        "audio/opus" = "mpv.desktop";
+        "audio/vorbis" = "mpv.desktop";
+        "audio/vnd.wav" = "mpv.desktop";
+        "audio/webm" = "mpv.desktop";
+        # image
+        "image/gif" = [
+          "qimgv.desktop"
+          "sxiv.desktop"
+        ];
+        "image/heic" = [
+          "qimgv.desktop"
+          "sxiv.desktop"
+        ];
+        "image/tiff" = [
+          "qimgv.desktop"
+          "sxiv.desktop"
+        ];
+        "image/svg+xml" = [
+          "qimgv.desktop"
+          "sxiv.desktop"
+        ];
+        "image/jpeg" = [
+          "qimgv.desktop"
+          "sxiv.desktop"
+        ];
+        "image/png" = [
+          "qimgv.desktop"
+          "sxiv.desktop"
+        ];
+        "image/webp" = [
+          "qimgv.desktop"
+          "sxiv.desktop"
+        ];
+        # text
+        "text/plain" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "text/csv" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "text/xml" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "text/html" = "firefox.desktop";
+        "text/markdown" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "text/css" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        "text/javascript" = [
+          "Helix.desktop"
+          "codium.desktop"
+        ];
+        # video
+        "video/3gpp" = "mpv.desktop";
+        "video/3gpp2" = "mpv.desktop";
+        "video/matroska" = "mpv.desktop";
+        "video/mp4" = "mpv.desktop";
+        "video/mpv" = "mpv.desktop";
+        "video/mpeg" = "mpv.desktop";
+        "video/ogg" = "mpv.desktop";
+        "video/webm" = "mpv.desktop";
+        "video/x-matroska" = "mpv.desktop";
+        # misc
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/chrome" = "firefox.desktop";
       };
     };
   };
