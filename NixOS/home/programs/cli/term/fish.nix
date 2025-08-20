dbd9d273565e6e7d580e244582b5f46414997670
ricdip <riccardodpr@gmail.com>
2025-08-21 01:06:30 +0200

    nixos: config

--- NixOS/home/programs/cli/term/fish.nix
+++ NixOS/home/programs/cli/term/fish.nix
@@ -18,7 +18,9 @@
       ls = "lsd -hAN --group-directories-first --color=always";
       grep = "grep --color=always";
       diff = "diff --color=always";
-      lsnet = "sudo netstat -tulpn";
+      # lsnet-listen = "sudo netstat -tulpn";
+      lsnet-listen = "sudo ss -tulpn";
+      lsnet-established = "sudo ss -antp state established";
       lsip = "curl ifconfig.me"; # print external IP address
       wifi-password = "nmcli device wifi show-password";
       wifi-list = "nmcli device wifi";
@@ -27,8 +29,8 @@
       wifi-connect = "nmcli --ask device wifi connect";
       gpg-encrypt = "gpg -c --no-symkey-cache --cipher-algo AES256";
       gpg-decrypt = "gpg -d --no-symkey-cache";
-      ps-cpu = "ps aux --sort=-pcpu | head -n 10";
-      ps-mem = "ps aux --sort=-pmem | head -n 10";
+      ps-cpu = "ps aux --sort=-pcpu | head -n 15";
+      ps-mem = "ps aux --sort=-pmem | head -n 15";
       ls-mount = "mount | column -t";
       c = "clear";
     };
