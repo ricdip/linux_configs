072e76b5ffdac4fec4bc1f8020f80cf79393e3d9
ricdip <riccardodpr@gmail.com>
2025-08-31 19:47:40 +0200

    nixos: config

--- NixOS/system/modules/services/ollama.nix
+++ NixOS/system/modules/services/ollama.nix
@@ -1,6 +1,9 @@
 {
   services.ollama = {
     enable = true;
+    # what interface to use for hardware acceleration
+    # acceleration = "cuda";
+    # download these models as soon as ollama.service has started (systemd unit ollama-model-loader.service)
     loadModels = [
       "dolphin-llama3:8b" # model with variety of instruction, conversational, and coding skills
       "gnokit/improve-prompt:latest" # model that improves user prompt for text-to-image generation
