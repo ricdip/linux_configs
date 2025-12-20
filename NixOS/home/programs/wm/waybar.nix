55dae4870d049ad79122b779f70d78db38cb1a22
ricdip <riccardodpr@gmail.com>
2025-12-20 19:08:02 +0100

    nixos: config

--- NixOS/home/programs/wm/waybar.nix
+++ NixOS/home/programs/wm/waybar.nix
@@ -2,15 +2,138 @@
 {
   programs.waybar = {
     enable = true;
+    style = ''
+      * {
+        /* `otf-font-awesome` is required to be installed for icons */
+        font-family: FontAwesome, Roboto, Helvetica, Arial, sans-serif;
+        font-size: 13px;
+      }
+
+      window#waybar {
+        background-color: rgba(43, 48, 59, 0.5);
+        border-bottom: 3px solid rgba(100, 114, 125, 0.5);
+        color: #ffffff;
+        transition-property: background-color;
+        transition-duration: .5s;
+      }
+
+      window#waybar.hidden {
+        opacity: 0.2;
+      }
+
+      window#waybar.termite {
+        background-color: #3F3F3F;
+      }
+
+      window#waybar.chromium {
+        background-color: #000000;
+        border: none;
+      }
+
+      button {
+        /* Use box-shadow instead of border so the text isn't offset */
+        box-shadow: inset 0 -3px transparent;
+        /* Avoid rounded borders under each button name */
+        border: none;
+        border-radius: 0;
+      }
+
+      /* https://github.com/Alexays/Waybar/wiki/FAQ#the-workspace-buttons-have-a-strange-hover-effect */
+      button:hover {
+        background: inherit;
+        box-shadow: inset 0 -3px #ffffff;
+      }
+
+      #workspaces button {
+        padding: 0 5px;
+        background-color: transparent;
+        color: #ffffff;
+      }
+
+      #workspaces button:hover {
+        background: rgba(0, 0, 0, 0.2);
+      }
+
+      #workspaces button.focused, #workspaces button.active {
+        background-color: #64727D;
+        box-shadow: inset 0 -3px #ffffff;
+      }
+
+      #workspaces button.urgent {
+        background-color: #eb4d4b;
+      }
+
+      #mode {
+        background-color: #64727D;
+        box-shadow: inset 0 -3px #ffffff;
+      }
+
+      #bluetooth,
+      #load,
+      #clock,
+      #battery,
+      #cpu,
+      #memory,
+      #disk,
+      #temperature,
+      #backlight,
+      #network,
+      #pulseaudio,
+      #wireplumber,
+      #custom-media,
+      #tray,
+      #mode,
+      #idle_inhibitor,
+      #scratchpad,
+      #power-profiles-daemon,
+      #language,
+      #mpd {
+        padding: 0 10px;
+        color: #ffffff;
+      }
+
+      #window,
+      #workspaces {
+        margin: 0 4px;
+      }
+
+      /* If workspaces is the leftmost module, omit left margin */
+      .modules-left > widget:first-child > #workspaces {
+        margin-left: 0;
+      }
+
+      /* If workspaces is the rightmost module, omit right margin */
+      .modules-right > widget:last-child > #workspaces {
+        margin-right: 0;
+      }
+
+      @keyframes blink {
+        to {
+          background-color: #ffffff;
+          color: #000000;
+        }
+      }
+
+      label:focus {
+        background-color: #000000;
+      }
+
+      #tray > .passive {
+        -gtk-icon-effect: dim;
+      }
+
+      #tray > .needs-attention {
+        -gtk-icon-effect: highlight;
+        background-color: #eb4d4b;
+      }
+    '';
     settings = {
       mainBar = {
         layer = "top";
         position = "top";
         height = 30;
         modules-left = [
-          "sway/workspaces"
-          "sway/mode"
-          "wlr/taskbar"
+          # "wlr/taskbar"
           "network"
           "bluetooth"
           "disk"
@@ -18,7 +141,9 @@
           "cpu"
         ];
         modules-center = [
-          "sway/window"
+          # "sway/window"
+          "sway/workspaces"
+          "sway/mode"
         ];
         modules-right = [
           "load"
