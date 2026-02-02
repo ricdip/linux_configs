3616981caef155eb0e204dd84340be4fa5f866ba
ricdip <riccardodpr@gmail.com>
2026-02-03 00:37:18 +0100

    nixos: config

--- NixOS/system/modules/services/btrfs.nix
+++ NixOS/system/modules/services/btrfs.nix
@@ -9,7 +9,7 @@
 
   services.snapper = {
     # snapshot interval: format https://www.freedesktop.org/software/systemd/man/latest/systemd.time.html
-    snapshotInterval = "hourly";
+    # snapshotInterval = "hourly";
     # cleanup interval: format https://www.freedesktop.org/software/systemd/man/latest/systemd.time.html
     cleanupInterval = "1d";
     configs = {
@@ -21,6 +21,8 @@
       #   TIMELINE_CREATE = true;
       #   # defines whether the timeline cleanup algorithm should be run for the config
       #   TIMELINE_CLEANUP = true;
+      #   # defines whether the number cleanup algorithm should be run for the config
+      #   NUMBER_CLEANUP = true;
       #   # limits for timeline cleanup
       #   TIMELINE_LIMIT_HOURLY = "2";
       #   TIMELINE_LIMIT_DAILY = "1";
@@ -33,8 +35,9 @@
         SUBVOLUME = "/home";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        NUMBER_CLEANUP = true;
         TIMELINE_LIMIT_HOURLY = "5";
-        TIMELINE_LIMIT_DAILY = "2";
+        TIMELINE_LIMIT_DAILY = "3";
         TIMELINE_LIMIT_WEEKLY = "1";
         TIMELINE_LIMIT_MONTHLY = "0";
         TIMELINE_LIMIT_YEARLY = "0";
